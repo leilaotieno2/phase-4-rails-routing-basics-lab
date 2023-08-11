3550964c5c3949acab0ec11a9104a6c29b2629d8
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-08-12 01:27:55 +0300

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'students/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+  get 'students/index'
+  get 'students', to: 'students#index'
+end
+
 end
