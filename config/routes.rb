5458ee790836957a6b6230e75f04cfaf4490b1d2
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-08-23 08:18:58 +0300

    updating the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'students/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-  get 'students/index'
+  get 'students/grades' ,to: 'students#grades'
   get 'students', to: 'students#index'
-end
+   end
 
-end
